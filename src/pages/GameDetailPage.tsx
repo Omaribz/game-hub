5f15e021d70212a2f105cf4b082c914c1a8770f4
Omaribz <cynthiamyud@gmail.com>
2023-11-01 14:14:46 +0300

    Build game screenshots

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -7,6 +7,7 @@ import DefinitionItem from "../Components/DefinitionItem";
 import CriticScore from "../Components/CriticScore";
 import GameAttributes from "../Components/GameAttributes";
 import GameTrailer from "../Components/GameTrailer";
+import GameScreenshots from "../Components/GameScreenshots";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -20,6 +21,7 @@ const GameDetailPage = () => {
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game} />
       <GameTrailer gameId={game.id} />
+      <GameScreenshots gameId={game.id} />
     </>
   );
 };
